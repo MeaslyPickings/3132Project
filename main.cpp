5e14dfc63ef526dd39eff6c3dc79731152f9de4a
Hugh <illogicaljp_211@hotmail.com>
2017-11-15 16:19:33 -0400

    Changes to main

--- main.cpp
+++ main.cpp
@@ -33,5 +33,61 @@ int main(void) {
 	// }
 	// cout << endl;
 
+	int playerNum;
+	
+	cout << "-----------------++ SORRY ++----------------------\n\n" << "Welcome to the game of SORRY. In order to player," << endl;
+																	 <<	"select the number of players. When it is your turn," << endl;
+																	 <<	"press the appropriate key to roll the dice and move\n" << endl;
+	while () { //user input selects continue
+		cout << "How many players are there? ----> ";
+		cin >> playerNum;
+		if (playerNum == 4) {
+			Players p1 = PLayers(1);
+			Players p2 = PLayers(2);
+			Players p3 = PLayers(3);
+			Players p4 = PLayers(4);
+			Board *b = new Board(p1, p2, p3, p4);
+			cout << "Start!\n" << endl;
+			b->startGame(p1);
+			b->startGame(p2);
+			b->startGame(p3);
+			b->startGame(p4);
+			while() { //still at least 2 players competing
+			
+			}
+			//declare winners and placements
+		}
+		else if (playerNum == 3) {
+			Players p1 = PLayers(1);
+			Players p2 = PLayers(2);
+			Players p3 = PLayers(3);
+			Board *b = new Board(p1, p2, p3);
+			cout << "Start!\n" << endl;
+			b->startGame(p1);
+			b->startGame(p2);
+			b->startGame(p3);
+			while() { //still at least 2 players competing
+			
+			}
+			//declare winners and placements
+		}
+		else if (playerNum == 2) {
+			Players p1 = PLayers(1);
+			Players p2 = PLayers(2);
+			Board *b = new Board(p1, p2);
+			cout << "Start!\n" << endl;
+			b->startGame(p1);
+			b->startGame(p2);
+			while() { //still at least 2 players competing
+			
+			}
+			//declare winners and placements
+		}
+		else {
+			cout << "ERROR, invalid number of players!" << endl;
+		}
+		//reset input
+	} 
+	
 	return 0;
 }
